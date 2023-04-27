796d3fc2b77c79a3c1d1ee679ff7948c18b6bd6a
Juan Calvopina <juan.calvopina@gmail.com>
2023-04-26 22:45:31 -0500

    Add User domain

---
+++ msvc-user/src/main/java/com/jcalvopinam/msvc/user/domain/User.java
@@ -0,0 +1,31 @@
+package com.jcalvopinam.msvc.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USER")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
