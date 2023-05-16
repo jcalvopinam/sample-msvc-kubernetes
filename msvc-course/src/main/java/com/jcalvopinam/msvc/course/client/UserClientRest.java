fe119648d409eae726abfc63776ccdbad920eb65
Juan Calvopina <juan.calvopina@gmail.com>
2023-05-15 21:16:51 -0500

    Update ports for msvc-course

--- msvc-course/src/main/java/com/jcalvopinam/msvc/course/client/UserClientRest.java
+++ msvc-course/src/main/java/com/jcalvopinam/msvc/course/client/UserClientRest.java
@@ -38,7 +38,7 @@ import java.util.List;
 /**
  * @author jcalvopinam <juan.calvopina@gmail.com>
  */
-@FeignClient(name = "msvc-user", url = "http://host.docker.internal:8001")
+@FeignClient(name = "msvc-user", url = "host.docker.internal:8001")
 public interface UserClientRest {
 
     @GetMapping(value = "/{id}")
